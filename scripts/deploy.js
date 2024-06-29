28ecc2118b797a9c27f90f6f1c9cf125f803ca22
ANSHUMAN ROSHAN <anshumanroshan1607@gmail.com>
2024-06-29 23:37:54 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,11 +8,11 @@ const hre = require("hardhat");
 
 async function main() {
   const initBalance = 1;
-  const Assessment = await hre.ethers.getContractFactory("Assessment");
-  const assessment = await Assessment.deploy();
+  const Assessment = await hre.ethers.getContractFactory("Frontend");
+  const assessment = await Assessment.deploy(initBalance);
   await assessment.deployed();
 
-  console.log(`A contract with balance of 100 VP deployed to ${assessment.address}`);
+  console.log(`A contract with balance of ${initBalance} eth deployed to ${assessment.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
